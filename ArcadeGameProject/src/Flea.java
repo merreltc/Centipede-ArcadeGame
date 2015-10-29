32572f35044912597a3500dc0c03ae54804aa78d
panfilwk <panfilwk@73b82540-19b5-467e-b98f-3fb32e5c406e>
2015-10-29 01:08:41 +0000

    Skeleton for Mushrooms and Enemies

---
+++ ArcadeGameProject/src/Flea.java
@@ -0,0 +1,67 @@
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+
+public class Flea extends Entity{
+	public Flea(World world, Point2D centerPoint) {
+		super(world, centerPoint);
+		// TODO Auto-generated constructor stub.
+	}
+
+	private final int VELOCITY = 2;
+	private final int SCORE_VALUE = 500;
+	
+	public void spawnMushroom(){
+		if(Math.random() > .75)
+			new Mushroom(this.getWorld(), this.getCenterPoint());
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub.
+		return null;
+	}
+
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub.
+		return null;
+	}
+
+	@Override
+	public void timePassed() {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	public void setIsPaused(boolean isPaused) {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	public boolean getIsPaused() {
+		// TODO Auto-generated method stub.
+		return false;
+	}
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	public void checkCollision(Entity entity) {
+		// TODO Auto-generated method stub.
+		
+	}
+}
+
---
+++ ArcadeGameProject/src/Scorpion.java
@@ -0,0 +1,53 @@
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+
+public class Scorpion extends Entity{
+	public Scorpion(World world, Point2D centerPoint) {
+		super(world, centerPoint);
+		// TODO Auto-generated constructor stub.
+	}
+	private final int VELOCITY = 1;
+	private int SCORE_VALUE = 1000;
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub.
+		return null;
+	}
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub.
+		return null;
+	}
+	@Override
+	public void timePassed() {
+		// TODO Auto-generated method stub.
+		
+	}
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub.
+		
+	}
+	@Override
+	public void setIsPaused(boolean isPaused) {
+		// TODO Auto-generated method stub.
+		
+	}
+	@Override
+	public boolean getIsPaused() {
+		// TODO Auto-generated method stub.
+		return false;
+	}
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub.
+		
+	}
+	@Override
+	public void checkCollision(Entity entity) {
+		// TODO Auto-generated method stub.
+		
+	}
+
+}
